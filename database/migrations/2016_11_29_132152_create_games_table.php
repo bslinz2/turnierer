e4e5d56fad77d8d2dd6e5079f74fee02fc6448fb
Frederic Köberl <fred@internetztube.net>
2016-11-29 13:25:57 +0100

    add games and teams tables migration

---
+++ database/migrations/2016_11_29_132152_create_games_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('group_id');
+            $table->integer('team_id');
+            $table->integer('vs_team_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('games');
+    }
+}
